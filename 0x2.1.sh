89a5681b6da26da83c87164fdf4a5a07e0ca295e
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-11-13 19:38:03 -0500

    Create 0x2.1.sh

---
+++ 0x2.1.sh
@@ -0,0 +1,27 @@
+
+
+
+
+
+#!/bin/bash
+
+
+if [ "$1" == "" ]
+echo "You forgot an IP Address!"
+echo "Syntax: ./ipsweep.sh 192.168.103"
+
+else
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+
+
+
+
+
+
+
+
+
+
+
