b928bd77d08ac318da879bee424fa4cf7ca8be41
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-11-24 21:52:25 -0500

    Update 0x3.1-py102.py

--- 0x3.1-py102.py
+++ 0x3.1-py102.py
@@ -1,1 +1,67 @@
+#!/bin/python3
+
+#importing
+
+print("importing is important!")
+
+
+import sys #system functions and parameters
+from datetime import datetime
+
+
+print(datetime.now())
+
+
+from datetime import datetime as dt #importing with alias
+
+print(dt.now())
+
+def nl():
+	print ('\n')
+
+nl()
+
+#advanced strings
+
+print("advanced strings")
+my_name = "nick"
+print(my_name[0]) #first initial
+print(my_name[-1]) #last inital
+
+sentence = "this is a sentence."
+
+print(sentence[:4]) #captures this
+print(sentence[-9:-1]) #can also go backwards
+print(sentence[-0]) #just the first inital 
+
+
+print(sentence.split()) #split by delimiter (space)
+
+sentence_split = sentence.split()
+sentence_join = " " .join(sentence_split)
+print(sentence_join)
+print ('\n'.join(sentence_split))
+
+
+quoteception = "I said i wil give all the money"
+print(quoteception)
+
+quoteception = "i said, \"give me all the money\"" #char escape
+print(quoteception)
+
+print ("A" in "Apple") #boolean True
+
+letter = "A"
+word = "Apple"
+
+
+
+
+
+
+
+
+
+
+
 
