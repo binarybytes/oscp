ec2bf82cd1f9da2f75023e6c50a85ea211daa675
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-11-27 20:44:03 -0500

    Update 0x3.2-py103.py

--- 0x3.2-py103.py
+++ 0x3.2-py103.py
@@ -1,1 +1,15 @@
 
+#ftp
+python -m SimpleHTTPServer 80 #python 2.0
+python3 -m http.server 80 #python 3.0
+
+
+#pyftpdlib
+#apt install python-pip
+#pip install pyftpdlib
+
+python -m pyftpdlib -p 21 -w #host ftp sites, -w flag is auth?!
+
+#great to no stop/start services all the time
+
+
