0c37b3650ed57f91515cb0db7f126374e469e5ac
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-11-27 21:21:45 -0500

    Update 0x4-shittyPortScanner.py

--- 0x4-shittyPortScanner.py
+++ 0x4-shittyPortScanner.py
@@ -1,1 +1,63 @@
+#!/bin/python3
+
+
+#v1 shitty portscanner by unix -- no threading
+
+import sys #cmd arguments++
+import socket  #connect to afinet and do soc streaming, port++
+from datetime import datetime
+
+#define target
+
+#python3 pscan.py <ip>
+
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #translate hostname to ipv4
+
+else:
+	print("invalid amound of argumentz!")
+	print("syntax: python3 scanner.py <ip>")
+	sys.exit()
+
+#banner++
+print("`" * 50)
+print("scanning target " +target)
+print("time started: "+str(datetime.now()))
+print("`" * 50) 
+
+
+try: 
+	for port in range(50,85):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#AF_INET is ipv4
+#sockstream is your port
+		socket.setdefaulttimeout(1) #float
+		result = s.connect_ex((target,port)) #returns error indicator
+#no error ++ 0
+		print("checking port {}".format((port))
+		if result == 0:
+			print("port {} is open".format(port))
+		s.close()
+
+
+except KeyboardInterrupt:
+	print("\nexiting program...")
+	sys.exit()
+
+except socket.gaierror:
+	print("hostname could not be resolved.")
+
+except socket.error:
+	print("couldnt connect to server")
+	sys.exit()
+
+
+
+
+
+
+
+
+
+
 
