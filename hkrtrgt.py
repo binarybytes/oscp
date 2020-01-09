0452af3d3a3f265ddbbd6d3fcc51669ebb1ce130
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-01-08 22:02:59 -0500

    Update hkrtrgt.py

--- hkrtrgt.py
+++ hkrtrgt.py
@@ -1,16 +1,19 @@
 #curl https://api.hackertarget.com/dnslookup/?q=test.xx.com
-/whois/?q=
-/geoip/?q=
-/reverseip/?q=
-/httpheaders/?q=
-/pagelinks/?q=
-/aslookup/?q=
+#/whois/?q=
+#/geoip/?q=
+#/reverseip/?q=
+#/httpheaders/?q=
+#/pagelinks/?q=
+#/aslookup/?q=
 
 #---------------------------------#
 #to do: create program to run this#
 #                                 #
 #                                 #
 #---------------------------------#
+
+import datetime from datetime
+import multiprocessing.pool import ThreadPool
 
 url = https://api.hackertarget.com/dnslookup/?q=
 
@@ -20,12 +23,15 @@ httpheaders = https://api.hackertarget.com/httpheaders/?q=
 pagelinks = https://api.hackertarget.com/pagelinks/?q=
 aslookup = https://api.hackertarget.com/aslookup/?q=
 
+
+  
 print("-"*50)
+print("start time: "+str(datetime.now()))
 print("-"*50)
 
 #add color somewhere doodz
 print("welcome...)
 
-def htr:
-  print=
+
+      
   
