8d71cdb1bd169ddea12b447ef9f9c298d3cb39b2
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-01-08 20:37:53 -0500

    Create hkrtrgt.py

---
+++ hkrtrgt.py
@@ -0,0 +1,31 @@
+#curl https://api.hackertarget.com/dnslookup/?q=test.xx.com
+/whois/?q=
+/geoip/?q=
+/reverseip/?q=
+/httpheaders/?q=
+/pagelinks/?q=
+/aslookup/?q=
+
+#---------------------------------#
+#to do: create program to run this#
+#                                 #
+#                                 #
+#---------------------------------#
+
+url = https://api.hackertarget.com/dnslookup/?q=
+
+geoip = https://api.hackertarget.com/geoip/?q=
+reverseip = https://api.hackertarget.com/reverseip/?q=
+httpheaders = https://api.hackertarget.com/httpheaders/?q=
+pagelinks = https://api.hackertarget.com/pagelinks/?q=
+aslookup = https://api.hackertarget.com/aslookup/?q=
+
+print("-"*50)
+print("-"*50)
+
+#add color somewhere doodz
+print("welcome...)
+
+def htr:
+  print=
+  
